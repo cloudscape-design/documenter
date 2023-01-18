3b0a619f954dff81ffca3e50e66c97b7f4eefcfa
Rúben Carvalho <rucarva@amazon.de>
2023-01-18 12:15:22 +0100

    Feature/deprecated-tag (#17)
    Co-authored-by: Ruben Carvalho <rucarva@amazon.com>

---
+++ fixtures/components/deprecated-tag/example/index.tsx
@@ -0,0 +1,34 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import * as React from 'react';
+
+export interface ExampleProps {
+  /**
+   * Header
+   *
+   */
+  header?: string;
+
+  /**
+   * Adds the specified classes to the root element of the component.
+   *
+   * @deprecated Custom CSS is not supported.
+   */
+  className?: string;
+
+  /**
+   * Main content
+   * @displayname content
+   * @deprecated This slot is not supported.
+   */
+  children?: React.ReactNode;
+}
+
+export default function Example({ header, className, children }: ExampleProps) {
+  return (
+    <div className={className}>
+      <header>{header}</header>
+      {children}
+    </div>
+  );
+}
---
+++ test/components/deprecated-tag.test.ts
@@ -0,0 +1,47 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { ComponentDefinition } from '../../src/components/interfaces';
+import { buildProject } from './test-helpers';
+
+let component: ComponentDefinition;
+beforeAll(() => {
+  const result = buildProject('deprecated-tag');
+  expect(result).toHaveLength(1);
+
+  component = result[0];
+});
+
+test('should have correct region and properties definitions', () => {
+  expect(component.properties).toEqual([
+    {
+      defaultValue: undefined,
+      deprecatedTag: 'Custom CSS is not supported.',
+      description: 'Adds the specified classes to the root element of the component.',
+      inlineType: undefined,
+      name: 'className',
+      optional: true,
+      type: 'string',
+      visualRefreshTag: undefined,
+    },
+    {
+      defaultValue: undefined,
+      deprecatedTag: undefined,
+      description: 'Header',
+      inlineType: undefined,
+      name: 'header',
+      optional: true,
+      type: 'string',
+      visualRefreshTag: undefined,
+    },
+  ]);
+  expect(component.regions).toEqual([
+    {
+      deprecatedTag: 'This slot is not supported.',
+      description: 'Main content',
+      displayName: 'content',
+      isDefault: true,
+      name: 'children',
+      visualRefreshTag: undefined,
+    },
+  ]);
+});
