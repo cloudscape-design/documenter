aadfc84149424b3b43873434fdde3942af3a37fb
Boris Serdiuk <serdiuk@amazon.com>
2025-07-21 11:23:14 +0200

    chore: Rewrite test utils doc generator (#77)

--- test/test-utils/usage.test.ts
+++ test/test-utils/usage.test.ts
@@ -5,24 +5,22 @@ import { buildTestUtilsProject } from './test-helpers';
 
 describe('documentTestUtils throws error for ', () => {
   test('failing project generation because of invalid config', () => {
-    expect(() => buildTestUtilsProject('errors-config')).toThrow('Errors during parsing configuration');
+    expect(() => buildTestUtilsProject('errors-config')).toThrow('Failed to parse tsconfig.json');
   });
 
   test('failing project generation because of faulty project files', () => {
-    expect(() => buildTestUtilsProject('errors-types')).toThrow('Project generation failed');
+    expect(() => buildTestUtilsProject('errors-types')).toThrow('Compilation failed');
   });
 
   test('having no input files because of the config', () => {
-    expect(() => buildTestUtilsProject('errors-empty')).toThrow('Errors during parsing configuration');
+    expect(() => buildTestUtilsProject('errors-empty')).toThrow('Failed to parse tsconfig.json');
   });
 
   test('having no input files because of a non-matching glob', () => {
-    expect(() => buildTestUtilsProject('simple', 'thisGlobWontMatchAnything')).toThrow('No input files to convert');
+    expect(() =>
+      buildTestUtilsProject('simple', {
+        domUtilsRoot: 'fixtures/does-not-exist/index.ts',
+      })
+    ).toThrow(/File '.*fixtures\/does-not-exist\/index.ts' not found/);
   });
-});
-
-test('glob works', () => {
-  const results = buildTestUtilsProject('glob-test', '**/a.ts');
-  expect(results.length).toBe(1);
-  expect(results[0].name).toBe('A');
 });
