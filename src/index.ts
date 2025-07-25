823d3ede465a26a3c63ae47e198d691658adb0c7
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-07-25 11:40:58 +0200

    chore: Exposes documentComponents util

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+
 export * from './components/interfaces';
-export { writeComponentsDocumentation } from './components';
+export { documentComponents, writeComponentsDocumentation } from './components';
 export { documentTestUtils } from './test-utils';
 export { writeTestUtilsDocumentation } from './test-utils-new';
