10af5d0dc1c0edfdc276ecd3bd138e8b7be4e76d
Boris Serdiuk <serdiuk@amazon.com>
2025-09-16 12:47:39 +0200

    chore: Upgrade typescript

--- test/components/type-utils.test.ts
+++ test/components/type-utils.test.ts
@@ -6,16 +6,17 @@ import { test, expect } from 'vitest';
 import { printFlags } from '../../lib/components/type-utils';
 
 test('serialises node flags', () => {
-  expect(
-    printFlags(
-      ts.NodeFlags.HasAsyncFunctions + ts.NodeFlags.HasImplicitReturn + ts.NodeFlags.ContainsThis,
-      ts.NodeFlags
-    )
-  ).toEqual('ContainsThis | HasImplicitReturn | ReachabilityCheckFlags | HasAsyncFunctions | ReachabilityAndEmitFlags');
+  const flags = printFlags(
+    ts.NodeFlags.HasAsyncFunctions + ts.NodeFlags.HasImplicitReturn + ts.NodeFlags.ContainsThis,
+    ts.NodeFlags
+  );
+  expect(flags).toContain('HasAsyncFunctions');
+  expect(flags).toContain('HasImplicitReturn');
+  expect(flags).toContain('ContainsThis');
 });
 
 test('serialises type flags', () => {
-  expect(printFlags(ts.TypeFlags.Number + ts.TypeFlags.Enum, ts.TypeFlags)).toEqual(
-    'Number | Enum | NumberLike | EnumLike | PossiblyFalsy | Primitive | NotPrimitiveUnion | Singleton | Intrinsic | IncludesMask | DisjointDomains | DefinitelyNonNullable | Narrowable'
-  );
+  const flags = printFlags(ts.TypeFlags.Number + ts.TypeFlags.Enum, ts.TypeFlags);
+  expect(flags).toContain('Number');
+  expect(flags).toContain('Enum');
 });
